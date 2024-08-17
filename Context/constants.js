b852edd3e4003d7265c48919c3924bba994888db
DaniTechDev <tochidan11@gmail.com>
2024-08-17 02:33:30 -0700

    Done with the TOKEN_ICO_ERC20 contract function

--- Context/constants.js
+++ Context/constants.js
@@ -74,7 +74,7 @@ export const ERC20 = async (address, userAddress) => {
       symbol: await contractReader.symbol(),
       address: await contractReader.address,
       totalSupply: toETH(await contractReader.totalSupply()),
-      balance: await toEth(contractReader.balanceOf(userAddress)),
+      balance: toETH(contractReader.balanceOf(userAddress)),
       contractTokenBalance: toETH(
         await contractReader.balanceOf(STAKING_DAPP_ADDRESS)
       ),
@@ -86,4 +86,89 @@ export const ERC20 = async (address, userAddress) => {
 
 //TOKEN ICO CONTRACT
 
-export const LOAD_TOKEN_ICO = async () => {};
+export const LOAD_TOKEN_ICO = async () => {
+  try {
+    const contract = await TOKEN_ICO_CONTRACT();
+    const tokenAddress = await contract.tokenAddress();
+
+    const tokenDetails = await contract.getTokenDetails();
+    const contractOwner = await contract.owner();
+    const soldTokens = await contract.soldTokens();
+
+    const ICO_TOKEN = await TOKEN_ICO_ERC20();
+
+    const token = {
+      tokenBal: ethers.utils.formatEther(tokenDetails.balance.toString()),
+      name: tokenDetails.name,
+      symbol: tokenDetails.symbol,
+      tokenPrice: ethers.utils.formatEther(tokenDetails.supply.toString()),
+      tokenAddr: tokenDetails.tokenAddr,
+      owner: contractOwner.toLowerCase(),
+      soldTokens: soldTokens.toNumber(),
+      token: ICO_TOKEN,
+    };
+
+    return token;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const TOKEN_ICO_CONTRACT = async () => {
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const { ethereum } = window;
+
+    if (ethereum) {
+      const signer = provider.getSigner();
+
+      const contractReader = new ethers.Contract(
+        TOKEN_ICO,
+        TokenICO.abi,
+        signer
+      );
+
+      return contractReader;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//this is function to get the ERC20 ICO created token by me. Any one that call
+//the function will get the information defined in the contract
+export const TOKEN_ICO_ERC20 = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+
+  try {
+    if (ethereum) {
+      const signer = provider.getSigner();
+
+      const contractReader = new ethers.Contract(
+        DEPOSIT_TOKEN,
+        CustomTokenABI.abi,
+        signer
+      );
+
+      //USER ADDRESS
+      const userAddress = await signer.getAddress();
+      const nativeBalance = await signer.getBalance();
+      const balance = await contractReader.balanceOf(userAddress);
+
+      const token = {
+        address: await contractReader.address,
+        name: await contractReader.name(),
+        symbol: await contractReader.symbol(),
+        decimals: await contractReader.decimals(),
+        supply: toETH(await contractReader.totalSupply()),
+        balance: toETH(contractReader.balanceOf(balance)),
+        contractTokenBalance: toETH(nativeBalance.toString()),
+      };
+
+      return token;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
