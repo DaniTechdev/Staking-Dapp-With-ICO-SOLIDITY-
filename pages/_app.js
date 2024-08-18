719d40bf99aa1eaf1b59438015f41d56587a6122
DaniTechDev <tochidan11@gmail.com>
2024-08-18 13:14:37 -0700

    made chain configuration for the provider

--- pages/_app.js
+++ pages/_app.js
@@ -10,10 +10,49 @@ import {
   midnightTheme,
 } from "@rainbow-me/rainbowkit";
 
+//NETWORK SETUP
+const HOLESKY = process.env.NEXT_PUBLIC_HOLESKY_RPC_URL;
+const EXPLORER = process.env.NEXT_PUBLIC_EXPLORER;
+const CHAIN_ID = process.env.NEXT_PUBLIC_CHAIN_ID;
+const CURRENCY = process.env.NEXT_PUBLIC_CURRENCY;
+const DECIMALS = process.env.NEXT_PUBLIC_NETWORK_DECIMALS;
+const NAME = process.env.NEXT_PUBLIC_NETWORK_NAME;
+const NETWORK = process.env.NEXT_PUBLIC_NETWORK;
+
+const TOKEN_ICO = process.env.NEXT_PUBLIC_TOKEN_ICO;
+
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
 export default function App({ Component, pageProps }) {
+  //NETWORKS
+  const { chains, provider } = configureChains([
+    {
+      id: Number(CHAIN_ID),
+      name: NAME,
+      network: NETWORK,
+      nativeCurrency: {
+        name: NAME,
+        symbol: CURRENCY,
+        decimals: DECIMALS,
+      },
+      rpcUrls: {
+        default: {
+          http: [`${HOLESKY}`],
+        },
+        public: {
+          http: [`${HOLESKY}`],
+        },
+      },
+      blockExplorers: {
+        default: {
+          name: "Holescan",
+          url: EXPLORER,
+        },
+      },
+      testnet: true,
+    },
+  ]);
   return (
     <>
       <Component {...pageProps} />
