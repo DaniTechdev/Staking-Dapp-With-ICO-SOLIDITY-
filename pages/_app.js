d61f4f1d77ee15000df00e0956ff5de89c47ce78
DaniTechDev <tochidan11@gmail.com>
2024-08-18 13:42:07 -0700

    Wrapped the entire component with the  wagmiConfig , RainbowKidProvider and the Toaster

--- pages/_app.js
+++ pages/_app.js
@@ -19,43 +19,77 @@ const DECIMALS = process.env.NEXT_PUBLIC_NETWORK_DECIMALS;
 const NAME = process.env.NEXT_PUBLIC_NETWORK_NAME;
 const NETWORK = process.env.NEXT_PUBLIC_NETWORK;
 
-const TOKEN_ICO = process.env.NEXT_PUBLIC_TOKEN_ICO;
-
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 
 export default function App({ Component, pageProps }) {
-  //NETWORKS
-  const { chains, provider } = configureChains([
-    {
-      id: Number(CHAIN_ID),
-      name: NAME,
-      network: NETWORK,
-      nativeCurrency: {
+  //NETWORKS and CHAIN configuration
+  const { chains, provider } = configureChains(
+    [
+      {
+        id: Number(CHAIN_ID),
         name: NAME,
-        symbol: CURRENCY,
-        decimals: DECIMALS,
-      },
-      rpcUrls: {
-        default: {
-          http: [`${HOLESKY}`],
+        network: NETWORK,
+        nativeCurrency: {
+          name: NAME,
+          symbol: CURRENCY,
+          decimals: DECIMALS,
         },
-        public: {
-          http: [`${HOLESKY}`],
+        rpcUrls: {
+          default: {
+            http: [`${HOLESKY}`],
+          },
+          public: {
+            http: [`${HOLESKY}`],
+          },
         },
-      },
-      blockExplorers: {
-        default: {
-          name: "Holescan",
-          url: EXPLORER,
+        blockExplorers: {
+          default: {
+            name: "Holescan",
+            url: EXPLORER,
+          },
         },
+        testnet: true,
       },
-      testnet: true,
+    ],
+    [
+      jsonRpcProvider({
+        rpc: (chain) => {
+          if (chain.id === Number(CHAIN_ID)) {
+            return { http: `${HOLESKY}` };
+          }
+          return null;
+        },
+        priority: 1,
+      }),
+    ]
+  );
+
+  const { connectors } = getDefaultWallets({
+    appName: "StakingDapp",
+    chains,
+  });
+
+  const wagmiClient = createClient({
+    autoConnect: true,
+    connectors,
+    provider,
+  });
+
+  const myTheme = merge(midnightTheme(), {
+    color: {
+      accentColor: "#562c7B",
+      accentColorForeground: "#fff",
     },
-  ]);
+  });
   return (
     <>
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chain} theme={myTheme}>
+          <Component {...pageProps} />
+          <Toaster />
+        </RainbowKitProvider>
+      </WagmiConfig>
 
       <script src="js/bootstrap.bundle.min.js"></script>
       <script src="js/smooth-scrollbar.js"></script>
