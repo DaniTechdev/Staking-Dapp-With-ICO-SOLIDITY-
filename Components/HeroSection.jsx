cc977b94882706e0e2556c3ee79ef0bc8c0b4fde
DaniTechDev <tochidan11@gmail.com>
2024-08-27 08:50:04 -0700

    Working on the HEROSECTION component

--- Components/HeroSection.jsx
+++ Components/HeroSection.jsx
@@ -1,6 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useAccount } from "wagmi";
 
-const HeroSection = () => {
+const CURRENCY = process.env.NEXT_PUBLIC_CURRENCY;
+
+import { TiTick } from "react-icons/ti";
+
+import { LOAD_TOKEN_ICO } from "../Context";
+
+const HeroSection = ({ poolDetails, addTokenMetaMask }) => {
+  const { address } = useAccount;
+
+  const [percentage, setPercentage] = useState();
+  const [tokenDetails, setTokenDetails] = useState();
+
+  useEffect(() => {
+    if (address) {
+      const loadToken = async () => {
+        const token = await LOAD_TOKEN_ICO();
+        setTokenDetails(token);
+      };
+    }
+
+    loadToken();
+  }, [address]);
+
+  useEffect(() => {
+    const calculatePercentage = () => {
+      const tokenSold = tokenDetails?.soldTokens ?? 0;
+
+      const tokenTotalSupply =
+        tokenDetails?.soldTokens + Number(tokenDetails?.tokenBal) * 1 ?? 1;
+
+      if (percentageNew == 0) {
+        console.log("Token balance is zero, cannot calculate percentage");
+      } else {
+        setPercentage(percentageNew);
+      }
+    };
+    const timer = setTimeout(calculatePercentage, 1000);
+
+    return () => clearTimeout(timer);
+  }, [notify]);
   return <div>HeroSection</div>;
 };
 
